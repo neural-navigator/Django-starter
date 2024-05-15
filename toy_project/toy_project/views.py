5187a7c3d9fae831eb27634f691bb75c1fe240b2
iamsatyapati <iamsatyapati@gmail.com>
2024-05-15 22:27:34 +0530

    template added

--- toy_project/toy_project/views.py
+++ toy_project/toy_project/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, world! You are at Neural-Navigator")
+    return render(request, 'website/index.html')
+    # return HttpResponse("Hello, world! You are at Neural-Navigator")
 
 
 def about(request):
