1a802cd7de90a00ff19fe908bcf06cc4d3da63ac
iamsatyapati <iamsatyapati@gmail.com>
2024-05-21 21:24:36 +0530

    urls updated to control the new app

--- toy_project/toy_project/urls.py
+++ toy_project/toy_project/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+    path('newapp/', include('newapp.urls'))
 ]
