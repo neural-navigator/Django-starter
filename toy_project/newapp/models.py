fdf4c809e65c7e14a36aa662214877c2c9ec7b43
iamsatyapati <iamsatyapati@gmail.com>
2024-05-26 19:57:55 +0530

    models added

--- toy_project/newapp/models.py
+++ toy_project/newapp/models.py
@@ -4,11 +4,11 @@ from django.utils import timezone
 # Create your models here.
 class NewVariety(models.Model):
     COLOR = [
-        (1, 'Blue'),
-        (2, 'Yellow'),
-        (3, 'Red')
+        ('1', 'Blue'),
+        ('2', 'Yellow'),
+        ('3', 'Red')
     ]
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='static_uploads/')
     created_on = models.DateField(default=timezone.now)
-    type = models.CharField(max_length=2, choices=COLOR)
+    type = models.CharField(max_length=1, choices=COLOR)
--- toy_project/newapp/views.py
+++ toy_project/newapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import NewVariety
 
 # Create your views here.
 def all_index(request):
     return render(request, 'newapp/all_index.html')
 
 
-
+def all_category(request):
+    variety = NewVariety.objects.all()
+    return render(request, 'newapp/all_index.html', {'veriety': 'veriety'})
