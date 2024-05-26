d6e27dbd2218a9bcc6d617553de0f0e1df9bb709
iamsatyapati <iamsatyapati@gmail.com>
2024-05-26 19:28:22 +0530

    models implemented

--- toy_project/newapp/models.py
+++ toy_project/newapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class NewVariety(models.Model):
+    COLOR = [
+        (1, 'Blue'),
+        (2, 'Yellow'),
+        (3, 'Red')
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='static_uploads/')
+    created_on = models.DateField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=COLOR)
